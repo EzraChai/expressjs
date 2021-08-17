f3587fc55a88dd1bbeaf599717efc6567f135f09
ezrachai <juanzhe2@gmail.com>
2021-08-17 19:22:54 +0800

    api

--- 07/app.js
+++ 07/app.js
@@ -13,6 +13,23 @@ app.get(/.*fly$/,function(req,res){
     res.send('/.*fly$/')
 })
 
+app.get('/user/:id/edit',(req,res) => {
+    res.status(200).json({Id : req.params.id})
+})
+
+// - .
+app.get('/flights/:from-:to',(req,res) => {
+    res.status(200).json({from : req.params.from , to : req.params.to})
+})
+
+app.get('/user/:userId(\\d+)',(req,res) => {
+    res.status(200).json({userId : req.params.userId})
+})
+
+app.use(function(req, res,next){
+    res.status(400).json({error: "Page Not Found."})
+})
+
 app.listen(4000 , ()=>{
     console.log("Server running at http://localhost:4000")
 })
